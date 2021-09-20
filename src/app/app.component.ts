a788aa2db1aa96fa91fcbcca2a2410c61791b419
Goran Trajkoski <goran.trajkoski97@gmail.com>
2021-09-21 01:35:34 +0200

    In Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'datingappweb';
+export class AppComponent implements OnInit {
+  title = 'The Dating App';
+ users : any;
+constructor( private accountService: AccountService) {}
+  ngOnInit(){
+
+ this.setCurrentUser();
+  }
+   setCurrentUser(){
+     const user: User = JSON.parse(localStorage.getItem('user'));
+     this.accountService.setCurrentUser(user);
+   }
+
 }
---
+++ src/app/shared.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import {TabsModule} from 'ngx-bootstrap/tabs';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    }),
+    TabsModule.forRoot(),
+    NgxGalleryModule
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule,
+    TabsModule,
+    NgxGalleryModule
+  ]
+})
+export class SharedModule { }
